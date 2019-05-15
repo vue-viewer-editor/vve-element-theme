2e0f6646471b2e329d343e22a4114bf7a86c73da
nianqin <bu__dong@163.com>
2019-05-15 15:18:09 +0800

    更换publishPath

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].js",
-    publicPath: "/dist"
+    publicPath: ""
   },
   plugins: [
     new HtmlWebpackPlugin({
